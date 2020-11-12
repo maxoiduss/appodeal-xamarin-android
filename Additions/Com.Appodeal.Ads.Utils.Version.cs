7af2813473f9054fe94d08d8e981e7bab86bc51a
Bass <maxoiduss@gmail.com>
2020-11-13 01:37:53 +0300

    Buildable proj and includable dll

---
+++ Additions/Com.Appodeal.Ads.Utils.Version.cs
@@ -0,0 +1,10 @@
+﻿namespace Com.Appodeal.Ads.Utils
+{
+	public partial class Version : global::Java.Lang.Object, global::Java.Lang.IComparable
+	{
+		int Java.Lang.IComparable.CompareTo(Java.Lang.Object obj)
+		{
+			return CompareTo((Com.Appodeal.Ads.Utils.Version)obj);
+		}
+	}
+}
